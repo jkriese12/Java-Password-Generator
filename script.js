893678bd545640b6abaf96d2766c1d2523ace8c9
Jacob Kriese <jkriese12@gmail.com>
2020-11-28 10:58:43 -0700

    Created variable arrays for values of password and created basic prompts for user to define what values they want to assign

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var abcCaps = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var abcLower = ["abcdefghijklmnopqrstuvwxyz"];
+var number = ["1234567890"];
+var special = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+
+console.log(abcLower);
+// Prompt questions for password parameters
+function generatePassword() {
+  prompt("Choose password length - Pick between 8 and 128 characters");
+ 
+  prompt("Do you want to include uppercase letters? YES or NO")
+ 
+  prompt("Do you want to unclude lowercase letters? YES or NO")
+ 
+  prompt("Do you want to include numbers? YES or NO")
+
+  prompt("Do you want to include special characters? YES or NO")
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
